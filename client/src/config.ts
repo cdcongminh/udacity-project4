aaa88cc565fc2b1158f1bf6223c49a276551b8f3
cdcongminh <cdcongminh@gmai.com>
2023-11-04 21:07:16 +0700

    add screenshot

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,8 +1,8 @@
-const apiId = 'fgbzj2ib48'
+const apiId = 'vq4gehppwb'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
-  domain: 'ddev-og0fq0jsyacx4ifd.us.auth0.com',                       // Auth0 domain
+  domain: 'dev-og0fq0jsyacx4ifd.us.auth0.com',                       // Auth0 domain
   clientId: '3XRIOnQqGTfev3NUq0V74elHmKMBAzVr',           // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
